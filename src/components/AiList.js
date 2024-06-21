cafdf37b4f05f7d3c7fc5509e08728a1814cde3e
poemfarer <poemfarer@gmail.com>
2024-06-21 13:41:39 +0900

    Ai Load complete

--- src/components/AiList.js
+++ src/components/AiList.js
@@ -1,81 +1,20 @@
-import styled from "styled-components"
-import React, { useEffect, useState } from 'react';
-import { getDoc, doc } from "firebase/firestore";
-import { db } from './firebaseConfig';
+import React from 'react'
+import AiItem from './AiItem'
 
 const AiList= ({data})=>{
    
     return(
         <>
-             <div>
-                {data.map((item) => (
-                    <div key={item.id}>
-                    <h2>{item.title}</h2>
-                    <p>{item.answer}</p>
-                    <p>{item.bookImg}</p>
-                    <p>{item.date && item.date.seconds ? new Date(item.date.seconds * 1000).toLocaleDateString() : ''}</p>
-                    <p>{item.passage}</p>
-                    <p>{item.question}</p>
-                    </div>
-                ))}
-                </div>
-            {/* {
-                data.slice(0, 10).map((value, index, array)=>{
-                    return <AiItem props={value} key={index}></AiItem>
-                })
-            } */}
-
-            {/* <AiItem props={value}></AiItem> */}
+            <div>
+            {Object.keys(data)
+            .sort((a, b) => data[b].date - data[a].date)
+            .map(docId => (
+                <AiItem key={docId} item={data[docId]} />
+                
+            ))}
+            </div>
         </>
-    //     <Content>
-    //         <p className='date'>작성일자: {data.date}</p>
-    //         <ExtractedText>메시지 내용 추출된 텍스트: {data.passage}</ExtractedText>
-    //             <QnA>
-    //                 <p>Q: {data.question}</p>
-    //                 <p>A: {data.answer}</p>
-    //             </QnA>
-    //     </Content>
     )
 }
 
 export default AiList
-
-const Content= styled.div`
-    border: 2px solid #6F4E37;
-    border-radius: 10px;
-    background-color: #FFFAED;
-    margin: 5%;
-
-    .date{
-        text-align: right;
-        margin-right: 5%;
-        font-size: 12px;
-        color: #5F5C5C;
-    }
-`
-
-const ExtractedText= styled.div`
-    background-color: #6F4E37;
-    color: white;
-    border: none;
-    border-radius: 10px;
-    padding: 10px;
-    max-height: 5rem;
-    margin: 5%;
-    overflow: auto;
-    margin-bottom: 1rem;
-    font-size: 11px;
-`
-
-const QnA= styled.div`
-    background-color: white;
-    /* background-color: #5E7E71; */
-    /* color: white; */
-    border: 1px solid black;
-    border-radius: 10px;
-    padding: 10px;
-    margin: 5%;
-    font-size: 14px;
-    max-height: 5rem;
-    overflow: auto;
-`
--- src/page/Ai.js
+++ src/page/Ai.js
@@ -4,181 +4,70 @@ import BookSlick from '../components/BookSlick';
 import SearchBar from '../components/SearchBar';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { initializeApp } from 'firebase/app';
-import { collection, doc, getDoc, getDocs } from "firebase/firestore"; 
+import { collection, doc, getDocs } from "firebase/firestore"; 
 import { db } from '../components/firebaseConfig';
 
+const Ai = () => {
+    const [aiData, setAiData] = useState();
 
-let id= "test"
-let isbn= '9788963717562'
-let date= '20240620'
-
-const fetchSubcollections = async (docRef) => {
-    const subcollections = [];
-    const collectionsSnapshot = await getDocs(collection(db, docRef.id));
-  
-    for (const subcollection of collectionsSnapshot.docs) {
-      const subcollectionPath = `${docRef.path}/${subcollection.id}`;
-      const subcollectionRef = collection(db, subcollectionPath);
-      const subcollectionSnapshot = await getDocs(subcollectionRef);
-  
-      for (const doc of subcollectionSnapshot.docs) {
-        if (doc.exists()) {
-          subcollections.push({
-            id: doc.id,
-            ...doc.data()
-          });
-  
-          const nestedSubcollections = await fetchSubcollections(doc.ref);
-          subcollections.push(...nestedSubcollections);
-        }
-      }
-    }
-  
-    return subcollections;
-  };
-  
-  const fetchData = async () => {
-    const docRef = doc(db, 'bert', id);
-  
-    try {
-      const docSnap = await getDoc(docRef);
-      let data = [];
-  
-      if (docSnap.exists()) {
-        data.push({ id: docSnap.id, ...docSnap.data() });
-  
-        const subcollections = await fetchSubcollections(docRef);
-        data.push(...subcollections);
-      } else {
-        console.log('No such document!');
-      }
-  
-      return data;
-    } catch (error) {
-      console.error('Error fetching document:', error);
-      return null;
-    }
-  };
-
-
-
-const Ai= ()=>{
-
-    const [aiData, setAiData]= useState([])
-
-    id= "test"
-
-    // const fetchData = async () => {
-    //     const docRef = doc(db, 'bert', id);
-      
-    //     const fetchSubcollections = async (docRef) => {
-    //       const collectionsSnapshot = await getDocs(collection(docRef));
-    //       const data = [];
-      
-    //       for (const subcollection of collectionsSnapshot.docs) {
-    //         const subDocRef = doc(docRef, subcollection.id);
-    //         const subDocSnap = await getDoc(subDocRef);
-    //         if (subDocSnap.exists()) {
-    //           data.push({
-    //             id: subcollection.id,
-    //             ...subDocSnap.data()
-    //           });
-      
-    //           const subcollections = await fetchSubcollections(subDocRef);
-    //           data.push(...subcollections);
-    //         }
-    //       }
-      
-    //       return data;
-    //     };
-      
-    //     try {
-    //       const docSnap = await getDoc(docRef);
-    //       let data = [];
-      
-    //       if (docSnap.exists()) {
-    //         data.push({ id: docSnap.id, ...docSnap.data() });
-      
-    //         const subcollections = await fetchSubcollections(docRef);
-    //         data.push(...subcollections);
-    //       } else {
-    //         console.log('No such document!');
-    //       }
-      
-    //       return data;
-    //     } catch (error) {
-    //       console.error('Error fetching document:', error);
-    //       return null;
-    //     }
-    //   };
-
-
-    // // 특정 date 문서의 모든 필드값을 가져오는 함수
-    // const fetchData = async () => {
-    //     try {
-    //       // Firestore에서 데이터 가져오기
-    //       const docRef = doc(db, 'bert', id, isbn, date);
-    //       const docSnap = await getDoc(docRef);
-    
-    //       if (docSnap.exists()) {
-    //         const data = docSnap.data();
-    //         alert(`Data: ${JSON.stringify(data, null, 2)}`);
-    //         console.log(data)
-    //         setAiData(data)
-
-    //       } else {
-    //         console.log("No such document!");
-    //       }
-    //     } catch (error) {
-    //       console.error("Error fetching document: ", error);
-    //     }
-    // };
+    const id= 'test'
 
     useEffect(() => {
-        const getData = async () => {
-          const data = await fetchData(id);
-          setAiData(data);
-          console.log('Fetched AI Data:', data); // 데이터 확인용 console.log
+        const fetchData = async () => {
+            try {
+                const bertDocRef = doc(db, 'bert', id); // 'bert' 컬렉션의 id 문서 참조
+                const booksCollectionRef = collection(bertDocRef, 'books'); // 'books' 서브컬렉션 참조
+                const querySnapshot = await getDocs(booksCollectionRef); // 참조의 모든 문서 가져오기
+                const data = {};
+
+                // 각 문서의 books 서브컬렉션을 반복
+                querySnapshot.forEach(doc => {
+                    data[doc.id] = doc.data();
+                });
+
+                setAiData(data);
+
+                console.log('Fetched AI Data:', data); // 데이터 확인용 console.log
+            } catch (error) {
+                console.error('Error fetching data: ', error);
+            }
         };
+
+        fetchData();
+    }, []);
+
+
     
-        getData();
-      }, [id]);
-
-    const [searchTerm, setSearchTerm]= useState('') 
-
-    const search= ()=>{
+    // 검색바
+    const [searchTerm, setSearchTerm] = useState('');
+    const search = () => {
         alert(searchTerm + "을/를 검색합니다")
     }
 
-    const navigate= useNavigate()
-
-    const addQnA= ()=>{
-        alert("QnA를 추가합니다")
-        navigate("../WriteAi")
+    // QnA 추가로 이동
+    const navigate = useNavigate();
+    const addQnA = () => {
+        alert("QnA를 추가합니다");
+        navigate("../WriteAi");
     }
-    
 
-    return(
+    return (
         <Container>
-            <Title >AI 독서 학습</Title>
-            <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} onClick={search} placeholder="검색어를 입력하세요"></SearchBar>
+            <Title>AI 독서 학습</Title>
+            <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} onClick={search} placeholder="검색어를 입력하세요" />
 
-            <div style={{textAlign:'center'}}>
-                <BookSlick></BookSlick>
+            <div style={{ textAlign: 'center' }}>
+                <BookSlick />
             </div>
-            <>
+
                 {aiData ? <AiList data={aiData}></AiList> : <p>loading</p>}
-                {/* {items.slice().reverse().map(()=>{
-                    return <AiList key={props.no} date={props.date} text={props.text} q={props.q} a={props.a}></AiList>
-                })} */}
-            </>
+
             <FloatingButton onClick={addQnA}>+</FloatingButton>
         </Container>
     )
 }
 
-export default Ai
+export default Ai;
 
 const Container= styled.div`
     display: flex;
@@ -190,24 +79,6 @@ const Container= styled.div`
     padding-left: 8%;
     padding-right: 8%;
 `
-
-const Search= styled.input`
-    text-align: center;
-    border: none;
-    background-color: #6F4E37;
-    border-radius: 15px;
-    height: 3rem;
-    color: white;
-    width: 100%;
-
-    &::placeholder{
-        color: white;
-        font-size: 1.2rem;
-    }
-`
-
-
-
 const FloatingButton = styled.button`
     position: fixed;
     bottom: 10%;
@@ -229,4 +100,3 @@ const Title= styled.p`
     text-align: center;
     
     margin-bottom: 20%;`
-    
