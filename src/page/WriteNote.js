22a5afd811d178aeadb33a7ce2b755e813dba9d9
hyun <chlguss97@gmail.com>
2024-06-23 23:22:29 +0900

    dd

--- src/page/WriteNote.js
+++ src/page/WriteNote.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import book from '../assets/book.png';
@@ -7,16 +7,22 @@ import icon2 from '../assets/icon2.png';
 import SaveBtn from '../components/SaveBtn';
 import BackBtn from '../components/BackBtn';
 import BookModal from '../components/BookModal';
-import BottomSheetModal from '../components/BottomSheetModal'
+import BottomSheetModal from '../components/BottomSheetModal';
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 8%;
+  padding-top: 8%;
+  padding-bottom: 20%;
+  padding-left: 8%;
+  padding-right: 8%;
   background-color: white;
-  height: 100vh;
+  height: 100%;
+  overflow-y: auto;
+  overflow-x: hidden;
   font-family: Arial, sans-serif;
+  position: relative;
 `;
 
 const Header = styled.div`
@@ -27,16 +33,17 @@ const Header = styled.div`
 `;
 
 const Content = styled.div`
-  width: 85%;
-  max-width: 400px;
+  width: 100%;
+  max-width: 600px;
   border: 2px solid #6F4E37;
   border-radius: 10px;
   padding: 20px;
   background-color: #FFFAED;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  overflow-y: auto;
-  height: calc(100vh - 350px);
-  margin-bottom: 20px;
+  margin-bottom: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const BookInfo = styled.div`
@@ -66,10 +73,17 @@ const BookAuthors = styled.div`
   color: #5F5C5C;
 `;
 
-const ActionButton = styled.button`
+const ActionButtons = styled.div`
+  display: flex;
+  justify-content: center;
   width: 100%;
+  margin-bottom: 20px;
+`;
+
+const ActionButton = styled.button`
+  flex: 1;
+  max-width: 150px;
   padding: 10px;
-  margin: 10px 0;
   border: 1px dashed #5F5C5C;
   background-color: white;
   font-size: 16px;
@@ -83,15 +97,31 @@ const ActionButton = styled.button`
 `;
 
 const NoteInput = styled.textarea`
-  width: 92%;
-  height: 60%;
+  width: 100%;
+  max-width: 550px;
+  height: 300px;
   margin-top: 20px;
   border: 1px solid #6F4E37;
   border-radius: 5px;
   padding: 10px;
-  font-size: 14px;
+  font-size: 16px;
   background: none;
-  &:focus { outline:2px solid #5E7E71; }
+  resize: none;
+  &:focus {
+    outline: 2px solid #5E7E71;
+  }
+`;
+
+const OCRTextBox = styled.div`
+  width: 100%;
+  max-width: 550px;
+  margin-top: 20px;
+  border: 1px solid #6F4E37;
+  border-radius: 5px;
+  padding: 10px;
+  font-size: 16px;
+  background: #FFF8DC;
+  white-space: pre-wrap; /* Preserve whitespace and line breaks */
 `;
 
 const Icon = styled.img`
@@ -104,6 +134,20 @@ const Icon = styled.img`
   align-items: start;
 `;
 
+const Video = styled.video`
+  width: 100%;
+  height: auto;
+  margin-bottom: 10px;
+  border: 1px solid #6F4E37;
+  border-radius: 5px;
+`;
+
+const ImageContainer = styled.div`
+  width: 100%;
+  max-width: 550px;
+  margin-bottom: 20px;
+`;
+
 const WriteNote = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [isBottomSheetOpen, setIsBottomSheetOpen] = useState(false);
@@ -112,20 +156,12 @@ const WriteNote = () => {
     authors: '',
     cover: book
   });
-
-  const openBottomSheet = () => {
-    setIsBottomSheetOpen(true);
-  };
-
-  const closeBottomSheet = () => {
-    setIsBottomSheetOpen(false);
-  };
-
-  const bookData = Array.from({ length: 20 }, (_, index) => ({
-    title: `트렌드 코리아 ${2024 - index}`,
-    authors: `저자 ${index + 1}`,
-    cover: "https://via.placeholder.com/150"
-  }));
+  const [ocrText, setOcrText] = useState('');
+  const [imageData, setImageData] = useState('');
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+  const [showWebcam, setShowWebcam] = useState(false);
+  const [isAIExtract, setIsAIExtract] = useState(false);
 
   const handleBookInfoClick = () => {
     setModalIsOpen(true);
@@ -141,32 +177,125 @@ const WriteNote = () => {
     navigate(-1);
   };
 
+  const handlePictureClick = () => {
+    setIsAIExtract(false);
+    setShowWebcam(true);
+  };
+
+  const handleAIExtractClick = () => {
+    setIsAIExtract(true);
+    setShowWebcam(true);
+  };
+
+  const takePicture = () => {
+    if (videoRef.current && canvasRef.current) {
+      const canvas = canvasRef.current;
+      const video = videoRef.current;
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+      const dataUrl = canvas.toDataURL('image/jpeg');
+      if (isAIExtract) {
+        sendImageToApp(dataUrl);
+      } else {
+        setImageData(dataUrl);
+      }
+      setShowWebcam(false); // 웹캠 종료
+    }
+  };
+
+  const sendImageToApp = (dataUrl) => {
+    if (window.Android) {
+      window.Android.receiveImage(dataUrl);
+    }
+  };
+
+  useEffect(() => {
+    if (showWebcam && navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: { facingMode: { exact: 'environment' } } })
+        .then(stream => {
+          if (videoRef.current) {
+            videoRef.current.srcObject = stream;
+            videoRef.current.play();
+          }
+        })
+        .catch(err => {
+          console.error('Error accessing the camera: ', err);
+        });
+    }
+
+    window.handleOCRResult = (result) => {
+      setOcrText(result); // OCR 결과를 새로운 텍스트 박스에 설정
+      setShowWebcam(false);
+    };
+  }, [showWebcam]);
+
+  useEffect(() => {
+    if (ocrText) {
+      // 기존 입력 내용에 OCR 결과를 추가
+      setOcrText((prevText) => prevText + '\n' + ocrText);
+    }
+  }, [ocrText]);
+
+  const bookData = Array.from({ length: 20 }, (_, index) => ({
+    title: `트렌드 코리아 ${2024 - index}`,
+    authors: `저자 ${index + 1}`,
+    cover: 'https://via.placeholder.com/150'
+  }));
+
   return (
     <Container>
       <BackBtn onClick={handleBackClick} />
       <Header>
         <BookInfo onClick={handleBookInfoClick}>
-          <BookImage src={selectedBook.cover} alt="Book Cover" />
+          <BookImage src={selectedBook.cover} alt='Book Cover' />
           <BookTitle>{selectedBook.title}</BookTitle>
           <BookAuthors>{selectedBook.authors}</BookAuthors>
         </BookInfo>
       </Header>
       <BottomSheetModal
         isOpen={isBottomSheetOpen}
-        onRequestClose={closeBottomSheet}
+        onRequestClose={() => setIsBottomSheetOpen(false)}
       />
-      <Content>
-        <ActionButton onClick={openBottomSheet} style={{cursor:"pointer"}}>
-          <Icon src={icon1} alt="Underline Icon" />
+      <ActionButtons>
+        <ActionButton onClick={handlePictureClick} style={{ cursor: 'pointer' }}>
+          <Icon src={icon1} alt='Underline Icon' />
           사진 & 하이라이트
         </ActionButton>
-        <ActionButton onClick={openBottomSheet} style={{cursor:"pointer"}}>
-          <Icon src={icon2} alt="Underline Icon"  />
+        <ActionButton onClick={handleAIExtractClick} style={{ cursor: 'pointer' }}>
+          <Icon src={icon2} alt='AI Extract Icon' />
           AI로 텍스트 추출
         </ActionButton>
-        <NoteInput placeholder="노트에 저장할 내용을 작성하세요" />
+      </ActionButtons>
+      {showWebcam && (
+        <div>
+          <Video ref={videoRef} autoPlay playsInline></Video>
+          <canvas ref={canvasRef} style={{ display: 'none' }}></canvas>
+          <ActionButton onClick={takePicture} style={{ cursor: 'pointer', margin: '20px auto' }}>
+            <Icon src={icon1} alt='Capture Icon' />
+            사진 찍기
+          </ActionButton>
+        </div>
+      )}
+      <Content>
+        {imageData && (
+          <ImageContainer>
+            <img src={imageData} alt='Captured' style={{ width: '100%', height: 'auto', marginBottom: '10px', border: '1px solid #6F4E37', borderRadius: '5px' }} />
+          </ImageContainer>
+        )}
+        {ocrText && (
+          <OCRTextBox>
+            {ocrText}
+          </OCRTextBox>
+        )}
+        <NoteInput
+          placeholder='노트에 저장할 내용을 작성하세요'
+          value={ocrText}
+          onChange={(e) => setOcrText(e.target.value)}
+        />
+        <SaveBtn name={'저장하기'} style={{ marginTop: '10px', marginBottom: '60px' }} />
       </Content>
-      <SaveBtn name={"저장하기"} />
       <BookModal
         isOpen={modalIsOpen}
         onRequestClose={() => setModalIsOpen(false)}
